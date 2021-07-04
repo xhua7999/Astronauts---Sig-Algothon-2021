3b430fa25a1b19a67d151e2b84e4892df20fc8cf
anniehfwx <anniehfwx@gmail.com>
2021-07-04 23:37:28 +1000

    added RSI Model

--- astronauts.py
+++ astronauts.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import math
 import numpy as np
+import pandas as pd
 from scipy.stats import linregress
 
 nInst=100
@@ -43,12 +44,42 @@ def applyEdgeModel (prcSoFar, theoPrices, currentPos): # theoPrices is a 100 len
             newPosition[i] = math.ceil(-10000/lastPrice[i]) # We sell as much as we allowed! Everything!
     return newPosition
 
+# Function to change current position based on the results of the RSI Model
+# Calculates the Relative Strength Index and uses the last RSI to determine the current stock position
+def applyRSIModel(prcSoFar, currentPos):
+    newPosition = currentPos
+
+    try:
+        lastPrice = prcSoFar[-1,:]
+    except:
+        # Handle Day 1 when prcSoFar is a 1D array
+        lastPrice = prcSoFar
+
+     # RSI Method
+    prcSoFar = pd.DataFrame(prcSoFar).T
+    RSI = prcSoFar.copy()
+    for i in range(100):
+        RSI[i] = computeRSI(prcSoFar[i], 14)
+        RSI_copy = RSI.to_numpy()
+       # print(f'stock #{17} RSI: {RSI_copy[17]}')
+        # Buy the stock as much as possible if RSI = 30
+        if RSI_copy[i][-1] < 20:
+            newPosition[i] = math.floor(10000/lastPrice[i])
+        # Sell if RSI = 70
+        elif RSI_copy[i][-1] > 70:
+            newPosition[i] = math.ceil(-10000/lastPrice[i])
+    
+    assert len(newPosition) == 100, "Position is incorrect"
+    return newPosition
+
 # Dummy algorithm to demonstrate function format.
 def getMyPosition (prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    rpos = np.array([int(x) for x in 1000 * np.random.randn(nins)])
-    currentPos += rpos
+    #rpos = np.array([int(x) for x in 1000 * np.random.randn(nins)])
+    #currentPos += rpos
+
+    currentPos = applyRSIModel(prcSoFar, currentPos)
     # The algorithm must return a vector of integers, indicating the position of each stock.
     # Position = number of shares, and can be positve or negative depending on long/short position.
     return currentPos
@@ -89,3 +120,29 @@ def momentum(prices):
     x = np.arange(len(returns))
     slope, _, rvalue, _, _ = linregress(x, returns)
     return ((1 + slope) ** 252) * (rvalue ** 2)
+
+# Calculating the RSI index for each stock 
+def computeRSI (data, time_window):
+    diff = data.diff(1).dropna()        # diff in one field(one day)
+
+    #this preservers dimensions off diff values
+    up_chg = 0 * diff
+    down_chg = 0 * diff
+    
+    # up change is equal to the positive difference, otherwise equal to zero
+    up_chg[diff > 0] = diff[ diff>0 ]
+    
+    # down change is equal to negative deifference, otherwise equal to zero
+    down_chg[diff < 0] = diff[ diff<0 ]
+    
+    # check pandas documentation for ewm
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.ewm.html
+    # values are related to exponential decay
+    # we set com=time_window-1 so we get decay alpha=1/time_window
+    up_chg_avg   = up_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    down_chg_avg = down_chg.ewm(com=time_window-1 , min_periods=time_window).mean()
+    
+    rs = abs(up_chg_avg/down_chg_avg)
+    rsi = 100 - 100/(1+rs)
+    return rsi
+
