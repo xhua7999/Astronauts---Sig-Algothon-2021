a02533611244dc1288a7135ea1dd122a8bb71dbc
David Young <d.young0851@gmail.com>
2021-07-07 01:42:41 +1000

    Added moving average bot and prelimnary parameter optimisation

--- analyse_data.py
+++ analyse_data.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import scipy
+from scipy.ndimage import gaussian_filter1d
+import random
 
 df = pd.read_csv('prices250.txt', sep='     ', header=None)
 print(df)
 
+NUM_DAYS = df.shape[0]
+NUM_STOCKS = df.shape[1]
+
 def convert_to_percentage_growth(df):
     percent_df = df.copy()
-    percent_df.loc[0,:] = np.zeros(len(df.loc[0,:]))
+    percent_df.iloc[0,:] = np.zeros(len(df.iloc[0,:]))
     for i in range(1, df.shape[0]):
-        percent_df.loc[i,:] = (df.loc[i,:] - df.loc[0,:]) / df.loc[0,:]
+        percent_df.iloc[i,:] = (df.iloc[i,:] - df.iloc[0,:]) / df.iloc[0,:]
     return percent_df
 
 percent_df = convert_to_percentage_growth(df)
@@ -17,7 +23,7 @@ print(percent_df)
 
 
 
-# final_return = (df.loc[df.shape[0]-1,:] - df.loc[0,:]) / df.loc[0,:]
+# final_return = (df.iloc[df.shape[0]-1,:] - df.iloc[0,:]) / df.iloc[0,:]
 # print(final_return)
 # print(sum(final_return) / len(final_return))
 
@@ -25,27 +31,70 @@ print(percent_df)
 
 def plot_df(df):
     for i in range(df.shape[1]):
-        data = df.loc[:,i]
+        data = df.iloc[:,i]
         plt.plot(data)
     plt.show()
+
 
 def get_std_dev(df):
     """Returns a list containing the standard deviations for every stock in the dataframe."""
     std_devs = []
     for i in range(df.shape[1]):
-        std_devs.append(np.std(df.loc[:,i].values))
+        std_devs.append(np.std(df.iloc[:,i].values))
     return std_devs
+
 
 def get_mean_price(df):
     means = []
     for i in range(df.shape[1]):
-        means.append(np.mean(df.loc[:,i].values))
+        means.append(np.mean(df.iloc[:,i].values))
     return means
 
-std_devs = get_std_dev(percent_df)
-mean_prices = get_mean_price(df)
 
-plt.scatter(mean_prices, std_devs)
-plt.xlabel('Average price of stock in dollars')
-plt.ylabel('Standard deviation in percentage increase from initial price')
-plt.show()
+def gaussian_filter(df, std_dev):
+    new_df = df.copy()
+    for i in range(df.shape[1]):
+        new_df.iloc[:,i] = gaussian_filter1d(df.iloc[:,i], std_dev)
+    return new_df
+
+
+def get_price_change(df):
+    new_df = df.copy()
+    new_df.iloc[0,:] = np.zeros(df.shape[1])
+    for i in range(1, df.shape[0]):
+        new_df.iloc[i,:] = df.iloc[i,:] - df.iloc[i-1,:]
+    return new_df
+
+
+def filter_stocks(df, keep_function):
+    drop_columns = []
+    for i in range(df.shape[1]):
+        if not keep_function(df.iloc[:,i].values):
+            drop_columns.append(i)
+    return df.drop(drop_columns, axis=1)
+
+
+percent_df = filter_stocks(percent_df, lambda arr: random.randint(1,30) == 1)
+plot_df(percent_df)
+
+filtered_percent_df = gaussian_filter(percent_df, 10)
+plot_df(filtered_percent_df)
+
+price_change_df = get_price_change(percent_df)
+plot_df(price_change_df)
+
+# means = get_mean_price(price_change_df)
+
+def total_price_change(price_change_df):
+    
+
+print(np.mean(means))
+
+
+# std_devs = get_std_dev(percent_df)
+# mean_prices = get_mean_price(df)
+
+# plt.scatter(mean_prices, std_devs)
+# plt.xlabel('Average price of stock in dollars')
+# plt.ylabel('Standard deviation in percentage increase from initial price')
+# plt.show()
