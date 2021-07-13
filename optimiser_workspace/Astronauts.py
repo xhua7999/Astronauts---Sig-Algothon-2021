ffd5b563dfada191dc11001bf9f5509db5ed5993
David Young <d.young0851@gmail.com>
2021-07-13 22:28:43 +1000

    updated moving average

--- optimiser_workspace/Astronauts.py
+++ optimiser_workspace/Astronauts.py
@@ -7,7 +7,7 @@ import numpy as np
 nInst=100
 currentPos = np.zeros(nInst)
 
-parameters = [0, 0, 0]
+parameters = [0.1, 0.55, 0.95]
 
 def moving_average(price_data, window_size):
     num_instruments, num_days = price_data.shape
@@ -22,24 +22,23 @@ def getMyPosition(prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
 
-    mov_avg1 = moving_average(prcSoFar, 2)
+    mov_avg1 = moving_average(prcSoFar, 1)
     mov_avg2 = moving_average(prcSoFar, 5)
-    mov_avg3 = moving_average(prcSoFar, 20)
+    mov_avg3 = moving_average(prcSoFar, 10)
 
     edge1 = (mov_avg1 - prcSoFar[:,-1]) / prcSoFar[:,-1]
     edge2 = (mov_avg2 - prcSoFar[:,-1]) / prcSoFar[:,-1]
     edge3 = (mov_avg3 - prcSoFar[:,-1]) / prcSoFar[:,-1]
 
     weighted_edge = parameters[0]*edge1 + parameters[1]*edge2 + parameters[2]*edge3
-
+    weighted_edge /= sum(parameters)
 
     required_edge = 0.005
-    currentPos = (10000/prcSoFar[:,-1]) * (1*(weighted_edge > required_edge) - 1*(weighted_edge < -required_edge))
-    # print(currentPos)
 
+    max_min_pos = (1*(weighted_edge > required_edge) - 1*(weighted_edge < -required_edge))
 
-    # rpos = np.array([int(x) for x in parameters[0] * np.ones(nins)])
-    # currentPos += rpos
+    currentPos = (10000/prcSoFar[:,-1]) * max_min_pos
+
     # The algorithm must return a vector of integers, indicating the position of each stock.
     # Position = number of shares, and can be positve or negative depending on long/short position.
     return currentPos
