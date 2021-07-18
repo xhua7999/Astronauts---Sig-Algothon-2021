3ca1bed9dfa387448c5bc6710611a1da63eac981
David Young <d.young0851@gmail.com>
2021-07-19 03:38:15 +1000

    pnl:8900, return:0.02078, sharpe:46.04

--- astronauts.py
+++ astronauts.py
@@ -5,21 +5,16 @@ import numpy as np
 nInst=100
 currentPos = np.zeros(nInst)
 
-# optimised_wma_window = [30, 41, 18, 42, 20, 47, 56, 44, 34, 7, 53, 49, 57, 32, 56, 53, 51, 44, 35, 36, 35, 52, 31, 59, 25, 49, 51, 24, 36, 24, 53, 46, 48, 26, 34, 42, 59, 56, 40, 33, 58, 36, 26, 28, 52, 58, 37, 15, 51, 32]
-# o2 = [19, 19, 19, 10, 20, 19, 19, 19, 19, 9, 19, 19, 19, 13, 19, 19, 15, 19, 19, 19, 19, 21, 19, 19, 19, 23, 14, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 21, 19, 40, 35, 19, 19, 18, 34, 59, 10, 19, 24]
-
 # Polynomial powers for moving average after optimisation
 optimised_powers = [2.250000000000001, 4.000000000000001, 6.300000000000002, 0.6000000000000002, 0.1, 0.45000000000000007, 2.0500000000000007, 2.450000000000001, 0.40000000000000013, 6.200000000000002, 1.2500000000000004, 1.6000000000000005, 7.750000000000002, 2.3000000000000007, 0.6500000000000001, 2.0500000000000007, 1.7500000000000007, 2.350000000000001, 0.8500000000000002, 3.2500000000000013, 3.8500000000000014, 1.5500000000000005, 3.550000000000001, 0.8000000000000002, 5.500000000000002, 2.800000000000001, 1.0500000000000003, 5.650000000000001, 3.2500000000000013, 5.850000000000001, 0.9500000000000003, 2.400000000000001, 3.9500000000000015, 5.050000000000002, 3.2500000000000013, 2.700000000000001, 1.4500000000000006, 0.9500000000000003, 0.30000000000000004, 2.0500000000000007, 4.850000000000001, 7.750000000000002, 6.150000000000001, 4.650000000000001, 0.9000000000000002, 1.3500000000000005, 1.0000000000000004, 3.9000000000000012, 0.30000000000000004, 5.600000000000001]
 optimised_powers = np.array(optimised_powers).T
 
-# Parameters used in our model and for optimisation
+# Parameters used in the model
 parameters = {
     "weighted_average_window_size": 19,
     "weighted_average_powers": optimised_powers,
     "required_edge": 0.005
 }
-
-
 
 # Array containing 1's for the stocks which we will trade and 0's for stocks we won't trade
 trade_stocks = np.ones((100,))
@@ -28,45 +23,41 @@ for i in range(50):
 
 
 def polynomial_moving_average(price_data, window_size, powers):
-    """Calculate the polynomial moving average of an array of price data."""
+    """Calculate the polynomial moving average of an array of price data.
+    A polynomial moving average is a weighted moving average where the 
+    weights of most recent prices are the highest, and successive weights
+    of past prices decay polynomially. The polynomial degree is determined
+    by the powers argument."""
+
     num_instruments, num_days = price_data.shape
 
+    # Initialise an array of linearly increasing weights between 0 and 1
     weights = np.linspace(0.01, 1, window_size)
 
+    # Adjust window size if is larger than the number of days of data
     if num_days < window_size:
         weights = weights[window_size - num_days:]
         window_size = min(window_size, num_days)        
 
+    # Duplicate the weights into a matrix such that there is a row of weights
+    # for each instrument
     weights = np.tile(weights, (num_instruments,1))
+
+    # Apply powers to the weights to attain polynomially increasing weights
     weights = np.power(weights, powers.reshape((num_instruments,1)))
 
+    # Normalise each instrument's weight to sum to 1
     weights /= np.sum(weights, axis=1).reshape(num_instruments,1)
 
+    # Multiply the prices by the weights for each day
     values = np.zeros((num_instruments, window_size))
     for i in range(window_size):
         values[:,-(1+i)] = price_data[:,-(1+i)] * weights[:,-(1+i)]
 
-    return np.sum(values, axis=1)
+    # Sum the results from each day to get the weighted moving average
+    moving_averages = np.sum(values, axis=1)
 
-# def non_vectorised_polynomial_moving_average(price_data, window_size, power):
-#     """Calculate the polynomial moving average of an array of price data."""
-#     num_instruments, num_days = price_data.shape
-    
-#     pma_values = np.zeros((num_instruments,1))
-
-#     for i in range(num_instruments):
-        
-#         window_size = optimised_wma_window[i]
-#         weights = np.linspace(0.01, 1, window_size)
-#         if num_days < window_size:
-#             weights = weights[window_size - num_days:]
-#             window_size = min(window_size, num_days)
-
-#         weights = np.power(weights, optimised_powers[i])
-#         weights /= np.sum(weights)
-
-#         for j in range(window_size):
-#             pma_values[-(1+j)] = price_data[i,-(1+j)] * weights[-(1+j)]
+    return moving_averages
 
 
 def getMyPosition(prcSoFar):
